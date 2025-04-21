f3a8022177d9ec85426dd162118187207c7f8345
Aixbox <1481051129@qq.com>
2025-04-21 16:18:49 +0800

    feat✨(project): 添加系统模块并配置相关依赖，添加SpringBoot启动类，启动项目
    - 新增 aixbox-common 和 aixbox-modules模块
    - 添加 Spring Boot相关依赖
    - 配置项目结构和模块依赖关系
    - 更新 pom.xml 文件以包含新模块

--- aixbox-admin/src/main/java/com/aixbox/web/AixboxApplication.java
+++ aixbox-admin/src/main/java/com/aixbox/web/AixboxApplication.java
@@ -1,8 +1,16 @@
 package com.aixbox.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * 启动程序
  */
-
+@SpringBootApplication
 public class AixboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AixboxApplication.class, args);
+    }
+
 }
