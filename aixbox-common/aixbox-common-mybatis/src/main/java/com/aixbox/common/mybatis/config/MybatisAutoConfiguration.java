e0edfa105c1030be2bb0903000e066ff42fa4520
Aixbox <1481051129@qq.com>
2025-04-25 16:09:02 +0800

    feat(common): 完成mybatis模块，新增通用返回对象和数据库相关功能
    - 新增 CommonResult 通用返回对象
    - 新增 BaseDO 基础实体类和 BaseMapperX 扩展 mapper 接口
    - 新增 DruidAdRemoveFilter 过滤器去除 Druid 底部广告- 新增 EncryptTypeHandler 字段加密 TypeHandler
    - 新增 IntegerListTypeHandler List<Integer> 类型转换器
    - 新增 JdbcUtils JDBC 工具类
    - 新增 DbTypeEnum 数据库类型枚举
    - 新增 ErrorCode错误码对象和 GlobalErrorCodeConstants 全局错误码枚举- 新增 DefaultDBFieldHandler通用字段填充处理器

---
+++ aixbox-common/aixbox-common-mybatis/src/main/java/com/aixbox/common/mybatis/config/MybatisAutoConfiguration.java
@@ -0,0 +1,47 @@
+package com.aixbox.common.mybatis.config;
+
+
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.parser.JsqlParserGlobal;
+import com.baomidou.mybatisplus.extension.parser.cache.JdkSerialCaffeineJsqlParseCache;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Mybatis 配置类
+ */
+@AutoConfiguration(before = MybatisPlusAutoConfiguration.class) // 目的：先于 MyBatis Plus 自动配置，避免 @MapperScan 可能扫描不到 Mapper 打印 warn 日志
+@MapperScan(value = "${aixbox.info.base-package}", annotationClass = Mapper.class,
+        lazyInitialization = "${mybatis.lazy-initialization:false}") // Mapper 懒加载，目前仅用于单元测试
+public class MybatisAutoConfiguration {
+
+    //todo 添加本地缓存优化
+    //static {
+    //    // 动态 SQL 智能优化支持本地缓存加速解析，更完善的租户复杂 XML 动态 SQL 支持，静态注入缓存
+    //    JsqlParserGlobal.setJsqlParseCache(new JdkSerialCaffeineJsqlParseCache(
+    //            (cache) -> cache.maximumSize(1024)
+    //                            .expireAfterWrite(5, TimeUnit.SECONDS))
+    //    );
+    //}
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor()); // 分页插件
+        return mybatisPlusInterceptor;
+    }
+
+    //todo 添加字段填充
+    //@Bean
+    //public MetaObjectHandler defaultMetaObjectHandler() {
+    //    return new DefaultDBFieldHandler(); // 自动填充参数类
+    //}
+
+}
