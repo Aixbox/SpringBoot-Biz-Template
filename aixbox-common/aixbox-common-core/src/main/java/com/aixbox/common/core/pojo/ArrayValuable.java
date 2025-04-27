b38f0f6ff6e9c51d622c50869b6a85a20e26ecd0
Aixbox <1481051129@qq.com>
2025-04-27 17:39:52 +0800

    feat(test): 新增单元测试基础框架
    - 重命名 DataSourceAutoConfiguration 为 AixboxDataSourceAutoConfiguration
    - 新增 ArrayValuable 接口和 CommonStatusEnum枚举
    - 添加断言工具类 AssertUtils 和随机数据生成工具类 RandomUtils
    - 新增 Redis 和 SQL 初始化的测试配置
    - 更新 AutoConfiguration 导入文件- 在 BOM 中添加相关依赖

---
+++ aixbox-common/aixbox-common-core/src/main/java/com/aixbox/common/core/pojo/ArrayValuable.java
@@ -0,0 +1,15 @@
+package com.aixbox.common.core.pojo;
+
+/**
+ * 可生成 T 数组的接口
+ *
+ * @author HUIHUI
+ */
+public interface ArrayValuable<T> {
+
+    /**
+     * @return 数组
+     */
+    T[] array();
+
+} 
--- aixbox-common/aixbox-common-mybatis/src/main/java/com/aixbox/common/datasource/config/AixboxDataSourceAutoConfiguration.java
+++ aixbox-common/aixbox-common-mybatis/src/main/java/com/aixbox/common/datasource/config/AixboxDataSourceAutoConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @AutoConfiguration
 @EnableTransactionManagement(proxyTargetClass = true) // 启动事务管理
 @EnableConfigurationProperties(DruidStatProperties.class)
-public class DataSourceAutoConfiguration {
+public class AixboxDataSourceAutoConfiguration {
 
 
     /**
