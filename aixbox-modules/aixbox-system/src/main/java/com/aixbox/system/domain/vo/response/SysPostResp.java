c7d262488e24a055eeed5d9f2b7b103947b3ed12
Aixbox <1481051129@qq.com>
2025-06-01 23:33:14 +0800

    feat(system): 添加岗位信息导出功能
    - 在 SysPostResp 类中添加了多个字段的 Excel属性注解
    - 增加了状态字段的字典格式化和转换注解
    - 添加了备注和创建时间字段的 Excel 属性注解

--- aixbox-modules/aixbox-system/src/main/java/com/aixbox/system/domain/vo/response/SysPostResp.java
+++ aixbox-modules/aixbox-system/src/main/java/com/aixbox/system/domain/vo/response/SysPostResp.java
@@ -1,5 +1,8 @@
 package com.aixbox.system.domain.vo.response;
 
+import com.aixbox.common.excel.annotation.ExcelDictFormat;
+import com.aixbox.common.excel.convert.ExcelDictConvert;
+import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -13,39 +16,49 @@ public class SysPostResp {
     /**
     * 岗位ID
     */
+    @ExcelProperty(value = "岗位序号")
     private Long id;
     /**
     * 部门id
     */
+    @ExcelProperty(value = "部门id")
     private Long deptId;
     /**
     * 岗位编码
     */
+    @ExcelProperty(value = "岗位编码")
     private String postCode;
     /**
     * 岗位类别编码
     */
+    @ExcelProperty(value = "类别编码")
     private String postCategory;
     /**
     * 岗位名称
     */
+    @ExcelProperty(value = "岗位名称")
     private String postName;
     /**
     * 显示顺序
     */
+    @ExcelProperty(value = "岗位排序")
     private Long postSort;
     /**
     * 状态（0正常 1停用）
     */
+    @ExcelProperty(value = "状态", converter = ExcelDictConvert.class)
+    @ExcelDictFormat(dictType = "sys_normal_disable")
     private String status;
-            /**
-    * 创建时间
-    */
-    private LocalDateTime createTime;
-                            /**
+    /**
     * 备注
     */
+    @ExcelProperty(value = "备注")
     private String remark;
+    /**
+     * 创建时间
+     */
+    @ExcelProperty(value = "创建时间")
+    private LocalDateTime createTime;
 
 
 }
