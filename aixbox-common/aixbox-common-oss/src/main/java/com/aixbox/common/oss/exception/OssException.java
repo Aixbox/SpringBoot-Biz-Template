dcb1d166e5087f9b557e9728cf1fbec69f3fd99a
Aixbox <1481051129@qq.com>
2025-06-09 21:14:07 +0800

    feat(common): 添加对象存储(OSS)功能
    - 新增 OSS客户端实现类 OssClient，支持多种云服务商
    - 添加 OSS 工厂类 OssFactory 用于创建和管理 OSS 客户端实例
    - 定义 OSS 配置属性 OssProperties
    - 实现文件上传、下载、删除等基本功能
    -支持预签名 URL 生成和自定义缓存策略

---
+++ aixbox-common/aixbox-common-oss/src/main/java/com/aixbox/common/oss/exception/OssException.java
@@ -0,0 +1,18 @@
+package com.aixbox.common.oss.exception;
+
+
+import java.io.Serial;
+
+/**
+ * OSS异常类
+ */
+public class OssException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public OssException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ aixbox-common/aixbox-common-oss/src/main/java/com/aixbox/common/oss/factory/OssFactory.java
@@ -0,0 +1,70 @@
+package com.aixbox.common.oss.factory;
+
+
+import com.aixbox.common.core.constant.CacheNames;
+import com.aixbox.common.core.utils.StrUtils;
+import com.aixbox.common.core.utils.json.JsonUtils;
+import com.aixbox.common.oss.constant.OssConstant;
+import com.aixbox.common.oss.core.OssClient;
+import com.aixbox.common.oss.exception.OssException;
+import com.aixbox.common.oss.properties.OssProperties;
+import com.aixbox.common.redis.utils.CacheUtils;
+import com.aixbox.common.redis.utils.RedisUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 文件上传Factory
+ */
+@Slf4j
+public class OssFactory {
+
+    private static final Map<String, OssClient> CLIENT_CACHE = new ConcurrentHashMap<>();
+    private static final ReentrantLock LOCK = new ReentrantLock();
+
+    /**
+     * 获取默认实例
+     */
+    public static OssClient instance() {
+        // 获取redis 默认类型
+        String configKey = RedisUtils.getCacheObject(OssConstant.DEFAULT_CONFIG_KEY);
+        if (StrUtils.isEmpty(configKey)) {
+            throw new OssException("文件存储服务类型无法找到!");
+        }
+        return instance(configKey);
+    }
+
+    /**
+     * 根据类型获取实例
+     */
+    public static OssClient instance(String configKey) {
+        String json = CacheUtils.get(CacheNames.SYS_OSS_CONFIG, configKey);
+        if (json == null) {
+            throw new OssException("系统异常, '" + configKey + "'配置信息不存在!");
+        }
+        OssProperties properties = JsonUtils.parseObject(json, OssProperties.class);
+        // 使用租户标识避免多个租户相同key实例覆盖
+        String key = configKey;
+        OssClient client = CLIENT_CACHE.get(key);
+        // 客户端不存在或配置不相同则重新构建
+        if (client == null || !client.checkPropertiesSame(properties)) {
+            LOCK.lock();
+            try {
+                client = CLIENT_CACHE.get(key);
+                if (client == null || !client.checkPropertiesSame(properties)) {
+                    CLIENT_CACHE.put(key, new OssClient(configKey, properties));
+                    log.info("创建OSS实例 key => {}", configKey);
+                    return CLIENT_CACHE.get(key);
+                }
+            } finally {
+                LOCK.unlock();
+            }
+        }
+        return client;
+    }
+
+
+}
