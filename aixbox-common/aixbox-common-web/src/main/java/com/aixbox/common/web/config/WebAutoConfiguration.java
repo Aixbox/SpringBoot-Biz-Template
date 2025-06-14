4de45726fdf748f5c2104e585ddba7b29190f375
Aixbox <1481051129@qq.com>
2025-06-14 20:39:34 +0800

    feat(aixbox-common-web): 添加跨域配置
    - 在 WebAutoConfiguration 类中添加了 CorsFilter Bean
    - 配置了允许跨域请求的来源、请求头、请求方法和最大存活时间
    - 通过 UrlBasedCorsConfigurationSource 设置全局跨域配置

--- aixbox-common/aixbox-common-web/src/main/java/com/aixbox/common/web/config/WebAutoConfiguration.java
+++ aixbox-common/aixbox-common-web/src/main/java/com/aixbox/common/web/config/WebAutoConfiguration.java
@@ -9,6 +9,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -47,6 +50,29 @@ public class WebAutoConfiguration implements WebMvcConfigurer {
         // 由于 WebFrameworkUtils 需要使用到 webProperties 属性，所以注册为一个 Bean
         return new WebUtils(webProperties);
     }
+
+    /**
+     * 跨域配置
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // 设置访问源地址
+        config.addAllowedOriginPattern("*");
+        // 设置访问源请求头
+        config.addAllowedHeader("*");
+        // 设置访问源请求方法
+        config.addAllowedMethod("*");
+        // 有效期 1800秒
+        config.setMaxAge(1800L);
+        // 添加映射路径，拦截一切请求
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        // 返回新的CorsFilter
+        return new CorsFilter(source);
+    }
+
 
 
 }
